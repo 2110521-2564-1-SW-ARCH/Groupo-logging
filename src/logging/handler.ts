84bbe30f5fdb966a18cd0d8ae0aac62bae638416
wongtawan-j <work@wongtawan.dev>
2021-09-28 02:58:17 +0700

    update logging service serializer

--- src/logging/handler.ts
+++ src/logging/handler.ts
@@ -4,20 +4,24 @@ import {serialize} from "./serializer";
 
 export const info: UntypedHandleCall = (call: ServerUnaryCall<ApplicationLogMessage, SerializedApplicationLogMessage>, callback: sendUnaryData<SerializedApplicationLogMessage>) => {
     const msg = serialize(call.request, "info");
+    console.log("[grpc-call]", msg);
     callback(null, {msg})
 }
 
 export const debug: UntypedHandleCall = (call: ServerUnaryCall<ApplicationLogMessage, SerializedApplicationLogMessage>, callback: sendUnaryData<SerializedApplicationLogMessage>) => {
     const msg = serialize(call.request, "warn");
+    console.log("[grpc-call]", msg);
     callback(null, {msg})
 }
 
 export const warn: UntypedHandleCall = (call: ServerUnaryCall<ApplicationLogMessage, SerializedApplicationLogMessage>, callback: sendUnaryData<SerializedApplicationLogMessage>) => {
     const msg = serialize(call.request, "warn");
+    console.log("[grpc-call]", msg);
     callback(null, {msg})
 }
 
 export const error: UntypedHandleCall = (call: ServerUnaryCall<ApplicationLogMessage, SerializedApplicationLogMessage>, callback: sendUnaryData<SerializedApplicationLogMessage>) => {
     const msg = serialize(call.request, "warn");
+    console.log("[grpc-call]", msg);
     callback(null, {msg})
 }
--- src/logging/serializer.ts
+++ src/logging/serializer.ts
@@ -6,13 +6,13 @@ export type LogLevel = "info" | "debug" | "warn" | "error";
 const levelColor: Record<LogLevel, string> = {info: "\x1b[32m", debug: "\x1b[34m", warn: "\x1b[31m", error: "\x1b[31m"}
 
 
-export const serialize = (message: ApplicationLogMessage, level: LogLevel): string => {
+export const serialize = (message: ApplicationLogMessage, level: LogLevel, service?: string): string => {
     let element: string[] = [];
 
     const d = dayjs();
 
     element.push(d.format("YYYY-MM-DD HH:mm:ss"));
-    element.push(`[${message.service}]`)
+    element.push(`[${service || message.service}]`)
 
     element.push(`${levelColor[level]}${level}\x1b[0m`);
 
@@ -25,7 +25,9 @@ export const serialize = (message: ApplicationLogMessage, level: LogLevel): stri
         fields.push(`\x1b[36m${k}\x1b[0m: \x1b[36m${message.fields[k]}\x1b[0m`);
     }
 
-    element.push("(" + fields.join(", ") + ")");
+    if (fields.length) {
+        element.push("[" + fields.join(", ") + "]");
+    }
 
     return element.join(" ");
 }
