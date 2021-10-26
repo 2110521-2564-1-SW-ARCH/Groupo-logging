78e10be95400ff1e9b3eaba075b7dc5ccba4b937
wongtawan-j <work@wongtawan.dev>
2021-10-26 20:50:35 +0700

    update

--- src/logging/handler.ts
+++ src/logging/handler.ts
@@ -8,28 +8,28 @@ const serializeBuffer = (log: ApplicationLogMessage): Buffer => {
 }
 
 export const info: UntypedHandleCall = (call: ServerUnaryCall<ApplicationLogMessage, SerializedApplicationLogMessage>, callback: sendUnaryData<SerializedApplicationLogMessage>) => {
-    publish(RabbitMQQueue, serializeBuffer(call.request));
+    // publish(RabbitMQQueue, serializeBuffer(call.request));
     const msg = serialize(call.request, "info");
     console.log("[grpc-call]", msg);
     callback(null, {msg})
 }
 
 export const debug: UntypedHandleCall = (call: ServerUnaryCall<ApplicationLogMessage, SerializedApplicationLogMessage>, callback: sendUnaryData<SerializedApplicationLogMessage>) => {
-    publish(RabbitMQQueue, serializeBuffer(call.request));
+    // publish(RabbitMQQueue, serializeBuffer(call.request));
     const msg = serialize(call.request, "debug");
     console.log("[grpc-call]", msg);
     callback(null, {msg})
 }
 
 export const warn: UntypedHandleCall = (call: ServerUnaryCall<ApplicationLogMessage, SerializedApplicationLogMessage>, callback: sendUnaryData<SerializedApplicationLogMessage>) => {
-    publish(RabbitMQQueue, serializeBuffer(call.request));
+    // publish(RabbitMQQueue, serializeBuffer(call.request));
     const msg = serialize(call.request, "warn");
     console.log("[grpc-call]", msg);
     callback(null, {msg})
 }
 
 export const error: UntypedHandleCall = (call: ServerUnaryCall<ApplicationLogMessage, SerializedApplicationLogMessage>, callback: sendUnaryData<SerializedApplicationLogMessage>) => {
-    publish(RabbitMQQueue, serializeBuffer(call.request));
+    // publish(RabbitMQQueue, serializeBuffer(call.request));
     const msg = serialize(call.request, "error");
     console.log("[grpc-call]", msg);
     callback(null, {msg})
